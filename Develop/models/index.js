7028a958ffb81d74bc99f8da27ea418de918d3f8
Sawako Goshima <sawako@192-168-1-103.tpgi.com.au>
2023-05-10 11:23:43 +1000

    Created env file and updated files in models folder

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,33 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongToMany(Tag, {
+  through: {
+    module: ProductTag,
+    unique: false
+  },
+  as: 'ProductLinkInTag'
+});
 
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongToMany(Product, {
+  through: {
+    module: ProductTag,
+    unique: false
+  },
+  as: 'TagLinkInProduct'
+});
 
 module.exports = {
   Product,
